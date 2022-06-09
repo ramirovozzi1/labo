5d91028918858f2320bb6ba0f7747e241ee7089e
ramirovozzi1 <ramiro.vozzi@genommalab.com>
2022-06-09 11:37:00 -0300

    ´

---
+++ exp/ST7610/Analisis6.R
@@ -0,0 +1,15 @@
+datos = read.delim("C:/Users/rvozzi/OneDrive - genommalabinternacional/Documentos/ECD/DataMining/labo/exp/ST7610/exp_ST7610_cluster_de_bajas_6.txt", header = TRUE, sep = "\t", dec = ".")
+datos$clase_ternaria<-NULL
+datos$cluster2 = factor(datos$cluster2)
+library(rpart)
+library(rpart.plot)
+mytree <- rpart(
+  cluster2 ~., 
+  data = datos, 
+  method = "class",
+  parms = list(split = 'information'), 
+  minsplit = 2, 
+  minbucket = 1,
+  #cp = 0
+)
+prp(mytree, extra=101, digits=5, branch=1, type=4, varlen=0, faclen=0)
